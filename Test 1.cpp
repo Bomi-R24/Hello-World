dc433e3adc422fe62595c2698331928fc3e385a0
Bomi-R24 <buomkuothkok@yahoo.com>
2024-10-08 18:29:43 +0300

    1st commit

---
+++ Test 1.cpp
@@ -0,0 +1,11 @@
+#include <iostream> //Include iostream
+using namespace std; // using standard namespace
+
+int main() //main function
+{
+    int number; //declare variable interger
+    cout <<"Enter a number"; //prompt user for input
+    cin >> number; //capture input
+    cout <<"You entered " + number << endl; //display output
+    cerr<< "This is an example of an error message"; //display error
+}
