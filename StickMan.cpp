340d40c0bb650a2ae4ae204d623ae8a3259c10aa
Bomi-R24 <buomkuothkok@yahoo.com>
2024-10-15 20:06:19 +0300

    15-Oct-2024 Class

---
+++ StickMan.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+
+cout<<"                             _0_    _0_ \n";
+cout<<"                            / | \\  / | \\ \n";
+cout<<"         ________          / _|_\\_/  |  \\ \n";
+cout<<"        |        |          /  \\     |     \n";
+cout<<" -------          -------  /____\\    /\\     \n";
+cout<<" |___0______________0____| _| |_    /  \\    \n";
+return 0;
+}
--- helloworld.cpp
+++ helloworld.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 
 using namespace std;
-int main()
-{
-    cout<<"Hello World. Glad to begin learning C++";
+int main() {
+    cout<<"Hello World. Glad to begin learning C++.";
     cout<<" My name is Bomi Ruea";
+    cout<<" This is my first C++ output statement";
+    cout<<" Soon, I will learn how to input data into my program"; 
+    cout<<"Strathmore University is the leading"<< endl;
+    cout<<"university in my region" << endl;
     return 0;
 }
